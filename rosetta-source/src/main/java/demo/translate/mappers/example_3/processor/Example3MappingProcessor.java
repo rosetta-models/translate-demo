8cd2a91ab870310c1d40e9dd4be383de9e9a89f7
hugohills <hugohills@hotmail.com>
2023-03-17 16:05:06 +0000

    Fix mapper example 3 and 4

--- rosetta-source/src/main/java/demo/translate/mappers/example_3/processor/Example3MappingProcessor.java
+++ rosetta-source/src/main/java/demo/translate/mappers/example_3/processor/Example3MappingProcessor.java
@@ -25,27 +25,21 @@ public class Example3MappingProcessor extends MappingProcessor {
      */
     @Override
     public void map(Path xmlPath, RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
-        // parameter: xmlPath = a->b->c->e (the path where the "mapper" syntax is specified)
-        // parameter: builder = ana instance of object FieldWithMetaString, where the value has been set to "CHIPS" (the value at the path where the mapper is specified)
-        // parameter: parent = an instance of object Z.ZBuilder that can be updated
-
-        Path parentXmlPath = xmlPath.getParent(); // a->b->c
-        Path newXmlPath = parentXmlPath.addElement("d"); // a->b->c->d (e.g. value of "FISH")
+        // parameter: xmlPath = engineType->engineDetail->metric->combustible (the path where the "mapper" syntax is specified)
+        // parameter: builder = an instance of object FieldWithMetaString, where the value has been set to "Gasoline" (the value at the path where the mapper is specified)
+        // parameter: parent = an instance of object EngineSpecificationBuilder that can be updated
 
         // this helper function will look up a xml path, and pass it to the consumer function, then updates the mapping stats
-        setValueAndUpdateMappings(newXmlPath,
+        setValueAndUpdateMappings(xmlPath,
                 // consumer function that takes value found at the xml path
                 xmlValueFromNewXmlPath -> {
                     // cast builder object
                     FieldWithMetaString.FieldWithMetaStringBuilder metaStringBuilder =
                             (FieldWithMetaString.FieldWithMetaStringBuilder) builder;
-                    // create new value to set
-                    String newValue = metaStringBuilder.getValue() + "_" + xmlValueFromNewXmlPath;
                     // set new value on builder object
                     metaStringBuilder
-                            .setValue(newValue)
                             .setMeta(MetaFields.builder()
-                                    .setScheme("foo-scheme-set-by-mapper"));
+                                    .setScheme("fuel-type-scheme"));
                 });
     }
 }
